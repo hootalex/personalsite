596898df98f1d3a843c274d0c4418df629cd91f3
Alex Tomlinson <alexdangersky@gmail.com>
2016-09-11 22:01:13 -0400

    Scrolling works in Firefox! Visited link color.

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function handler(event) {
 $(function(){
 		$("#page-wrap").wrapInner("<table cellspacing='30'><tr>");
 		$(".post").wrap("<td></td>");
-		$("body").mousewheel(function(event, delta) {
+		$('html, body').mousewheel(function(event, delta) {
 			this.scrollLeft -= (delta * 30);
 			event.preventDefault();
 		});
