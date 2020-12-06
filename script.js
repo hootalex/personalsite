ad8d7646b7945afafffe303706a2831cf26d3404
Alex Tomlinson <1alextomlinson@gmail.com>
2020-12-05 20:38:15 -0500

    Removed Google analytics

--- script.js
+++ script.js
@@ -37,13 +37,13 @@ changeFavicon('https://aelx.co/favicon2.ico');
  * Requires: 1.2.2+
  */
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-78729250-1', 'auto');
-  ga('send', 'pageview');
+  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  //
+  // ga('create', 'UA-78729250-1', 'auto');
+  // ga('send', 'pageview');
 
 
 (function($) {
